5b873cd4b2c5811ee9820c5bd2b86c80950a0232
Nicolas Goy <kuon@goyman.com>
2014-11-26 18:29:33 +0100

    Documentation

--- lib/swift_storage/container_collection.rb
+++ lib/swift_storage/container_collection.rb
@@ -2,10 +2,29 @@ class SwiftStorage::ContainerCollection < SwiftStorage::Node
 
   parent_node          :service
 
+
+  # Return all containers
+  #
+  # @note This method will return only the first 1000 containers.
+  #
+  # @return [Array<SwiftStorage::Container>]
+  #  Containers in this collection.
+  #
   def all
     get_lines('').map { |name| SwiftStorage::Container.new(service, name)}
   end
 
+  # Return a particular container
+  #
+  # @note This always return a container, regadeless of it's existence
+  #  on the server. This call do NOT contact the server.
+  #
+  # @param name [String]
+  #  The name (sometimes named key) of the container
+  #
+  # @return [SwiftStorage::Object]
+  #  Container with given name
+  #
   def [](name)
     SwiftStorage::Container.new(service, name)
   end
