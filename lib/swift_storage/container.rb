7f522624c81dd56c07ed3a9df146fbf535419578
Nicolas Goy <kuon@goyman.com>
2014-11-26 18:44:07 +0100

    More documentation

--- lib/swift_storage/container.rb
+++ lib/swift_storage/container.rb
@@ -11,14 +11,35 @@ class SwiftStorage::Container < SwiftStorage::Node
                              :object_count
 
 
+  # Returns the object collection for this container
+  #
+  # @return [SwiftStorage::ObjectCollection]
+  #  The object collection.
+  #
   def objects
     @objects ||= SwiftStorage::ObjectCollection.new(self)
   end
 
+  # Create the container
+  #
   def create
     request(relative_path, :method => :put)
   end
 
+  # Write the container meta data
+  #
+  # @note This overrides all ACL set on the container.
+  #
+  # Each ACL is a string in the following format:
+  #
+  # - `team:jon` give access to user "jon" of account "team"
+  #
+  # @param read_acl [Array<String>]
+  #  An array of ACL.
+  #
+  # @param write_acl [Array<String>]
+  #  An array of ACL.
+  #
   def write(write_acl: nil, read_acl: nil)
     h = {}
     read_acl = read_acl.join(',') if read_acl.respond_to?(:to_ary)
@@ -30,6 +51,11 @@ class SwiftStorage::Container < SwiftStorage::Node
     request(relative_path, :method => :post, :headers => h)
   end
 
+  # Read the container meta data
+  #
+  # @return [Struct]
+  #  A struct with `read` and `write` ACL, each entry contains an Array of
+  #  String.
   def acl
     r = headers.read.split(',') rescue nil
     w = headers.write.split(',') rescue nil
